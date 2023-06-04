3d92f41cb13445f8ad30e5506eeedbc40f292304
mj1999 <mayank.joshi92@yahoo.com>
2023-06-04 16:03:46 +0530

    added users route and users controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,6 @@
+module.exports.profile = function(req,res){
+    res.end('<h1>User profile</h1>')
+}
+module.exports.friendList = function(req,res){
+    res.end('<h1>My friends </h1>')
+}
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users_controller');
+
+router.get('/profile',usersController.profile);
+router.get('/friends',usersController.friendList);
+
+module.exports = router;
